4193a6d76de4b4c49e1d026d6fb2e2fb3826ab1d
Your Name <dat054760@gmail.com>
2022-07-28 02:04:34 +0700

    up

---
+++ src/Rectangle.java
@@ -0,0 +1,23 @@
+public class Rectangle {
+    double width, height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return width * height / 2;
+    }
+
+    public String display() {
+        return "Rectangle {" + "width: " + width + "," + " height:" + height + "}";
+    }
+}
